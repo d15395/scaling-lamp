34c1745f9e8e6076283190fae68af2697457b9c2
Sushmitha D Rao <drao15395@gmail.com>
2020-10-03 15:54:43 +0530

    Converting to a python facet and creating base module

---
+++ clean_code/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = 'clean_code python'
+__description__ = 'Read and document clean coding practices for python'
+__version__ = '0.2.0'
+__author__ = 'Sushmitha Rao'
+__author_email__ = 'cassini140917@gmail.com'
+__license__ = 'Apache 2.0'
+__url__ = 'https://github.com/AlexIoannides/py-package-template'
